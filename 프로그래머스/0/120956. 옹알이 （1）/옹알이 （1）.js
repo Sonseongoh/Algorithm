f0373cefaf0a54eb2b56f1567803dbc39e953c88
Sonseongoh <nobleser@naver.com>
2024-06-04 10:40:22 +0900

    [level 0] Title: 옹알이 (1), Time: 0.24 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.js
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.js
@@ -3,12 +3,16 @@ function solution(babbling) {
     let answer = 0;
     for (let x of babbling) {
         for (let y of word) {
-            x = x.split(y).join(' ');
+         if (x.includes(y)) {
+                 x = x.split(y).join(' ');
+            }
+            // console.log(x)
         }
       
         if (x.trim() === '') {
             answer++;
         }
+        // console.log(x)
     }
     
     return answer
@@ -16,3 +20,5 @@ function solution(babbling) {
 
 // 1.babbling 에 word 발음이 있는지 확인(for,includes)
 // 2.있다면 해당 단어를 제거해서 모두 제거 되면 정답 +1
+
+// x = x.split(y).join(' ');  이부분을 x = x.split(y).join(''); 으로 공백없이 작성했었더니   if (x=== '') 아래 if문을 이렇게 작성하니까 실패했었음
