624c622f073a1d894cf5fc995a91ca34b0c067a9
Sonseongoh <nobleser@naver.com>
2024-06-13 10:24:09 +0900

    [level 1] Title: 폰켓몬, Time: 0.81 ms, Memory: 33.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -0,0 +1,16 @@
+// 
+
+function solution(nums) {
+    let answer = [];
+    let max = nums.length / 2;
+        
+    for(let i = 0; i < nums.length; i++) {
+        if(answer.length < max) {
+            if(!answer.includes(nums[i])) {
+                answer.push(nums[i]);
+            }
+        }
+    }
+    
+    return answer.length;    
+}
