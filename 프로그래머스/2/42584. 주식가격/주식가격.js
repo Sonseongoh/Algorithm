436a3651b57f6efc6170a8953e489acb9370b8cf
Sonseongoh <nobleser@naver.com>
2024-11-17 23:34:42 +0900

    [level 2] Title: 주식가격, Time: 6.15 ms, Memory: 45.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42584. 주식가격/주식가격.js
@@ -0,0 +1,17 @@
+function solution(prices) {
+    let answer = [];
+    for(let i=0; i<prices.length; i++){
+        let count=0;
+        for(let j=i+1; j<prices.length; j++){
+            count++
+            if(prices[i]>prices[j]){
+                break;
+            }
+        }
+        answer.push(count)
+    }
+    return answer;
+}
+
+
+//1. prices[i]의 숫자++
