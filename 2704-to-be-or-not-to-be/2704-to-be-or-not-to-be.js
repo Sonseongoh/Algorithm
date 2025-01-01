bb212c4285315cd994843ad444533f2fa3393da4
Son <nobleser@naver.com>
2025-01-01 23:37:07 +0900

    Time: 52 ms (58%), Space: 48.6 MB (77.05%) - LeetHub

---
+++ 2704-to-be-or-not-to-be/2704-to-be-or-not-to-be.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {
+     const isEqual = (val, target) => {
+        return val === target;
+    }
+
+    const ERROR_MESSAGE = {
+        TO_BE: "Not Equal",
+        NOT_TO_BE: "Equal",
+    }
+
+    return{
+        toBe:function(target){
+            if(isEqual(val,target)) return true
+            throw new Error(ERROR_MESSAGE.TO_BE);
+        },
+        notToBe:function(target){
+             if(!isEqual(val, target)) return true;
+              throw new Error(ERROR_MESSAGE.NOT_TO_BE);
+        }
+    }
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
