20e4016de64f5acd34c61100aafd9ee156c1f82c
Son <nobleser@naver.com>
2025-05-15 23:14:58 +0900

    Title: DateTime to DateTime, Time: 192ms, Memory: 15MB, Status: Runtime Error - Codetree

--- 250515/DateTime to DateTime/datetime-to-datetime.js
+++ 250515/DateTime to DateTime/datetime-to-datetime.js
@@ -5,8 +5,13 @@ const [A, B, C] = input[0].split(' ').map(Number);
 
 // Please write your code here.
 
+
 let start=(11*24*60)+(11*60)+11
 
 let end=A*24*60+B*60+C
+
+if(end-start<0 return -1)
+
+
 
 console.log(end-start)
