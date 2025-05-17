4730d8b9c64dbbaf1a026c0092dcadef17ae96db
Son <nobleser@naver.com>
2025-05-18 02:42:00 +0900

    Title: 최대로 겹치는 구간, Time: 181ms, Memory: 15MB, Status: Passed - Codetree

---
+++ 250518/최대로 겹치는 구간/maximum-overlapped-segments.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(input[0]);
+const segments = [];
+for (let i = 1; i <= n; i++) {
+    segments.push(input[i].split(' ').map(Number));
+}
+
+// Please write your code here.
+
+const OFFSET = 100;
+const array = new Array(202).fill(0);
+
+for(let i=0; i<n; i++){
+    const[a,b]=segments[i]
+    for(let j=a; j<b; j++){
+        array[j+OFFSET]+=1
+    }
+}
+
+console.log(Math.max(...array))
