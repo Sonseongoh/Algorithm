3adb9f8ce79ca6a35c19d766ce74f2cf8500a645
Son <nobleser@naver.com>
2025-05-19 20:39:07 +0900

    Title: 사각형의 총 넓이 2, Time: 202ms, Memory: 18MB, Status: Passed - Codetree

--- 250519/사각형의 총 넓이 2/total-width-of-a-rectangle2.js
+++ 250519/사각형의 총 넓이 2/total-width-of-a-rectangle2.js
@@ -5,15 +5,26 @@ const n = Number(input[0]);
 const rects = input.slice(1, n + 1).map(line => line.split(' ').map(Number));
 
 // Please Write your code here.
+let answer=0
 const OFFSET=100
 let array=Array.from({length:202},()=>new Array(202).fill(0))
 
 
 for(let i=0; i<n; i++){
     const[x1,y1,x2,y2]=rects[i]
-    
-
+    for(let x=x1; x<x2; x++){
+        for(let y=y1; y<y2;y++){
+            array[x+OFFSET][y+OFFSET]=1
+        }
+    }
 }
 
+for(let i=0; i<202; i++){
+    for(let j=0; j<202; j++){
+        if(array[i][j]===1){
+            answer++
+        }
+    }
+}
 
-console.log(array2)
+console.log(answer)
