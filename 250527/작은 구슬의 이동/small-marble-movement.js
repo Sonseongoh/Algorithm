d0e606fec69562cec50808766444b9bf67b29dc1
Son <nobleser@naver.com>
2025-05-27 13:25:55 +0900

    Title: 작은 구슬의 이동, Time: 183ms, Memory: 15MB, Status: Runtime Error - Codetree

---
+++ 250527/작은 구슬의 이동/small-marble-movement.js
@@ -0,0 +1,35 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const [n, t] = input[0].split(" ").map(Number);
+let [r, c, d] = input[1].split(" ");
+r = Number(r);
+c = Number(c);
+
+// Please Write your code here.
+
+let dx=[-1,1,0,0]
+let dy=[0,0,1,-1]
+
+let dir={
+    'U':0,
+    'D':1,
+    'R':2,
+    'L':3
+}
+
+let dirIdx=dir[d]
+
+for(let i=0; i<t; i++){
+    let nx=r+dx[dirIdx]
+    let nc=r+dy[dirIdx]
+    if(nx<1||nx>n||ny<1||ny>n){
+        if(dirIdx===0){
+            dirIdx=1
+        }else if (dIdx === 1) {
+        dIdx = 0;
+        }else if (dIdx === 2) {
+            dIdx = 3;
+        else if (dIdx === 3) dIdx = 2;
+    }
+}
