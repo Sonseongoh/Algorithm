bd64f81bc30a2ad5798bd1903f12d9bd1586848e
Son <nobleser@naver.com>
2025-05-27 13:33:59 +0900

    Title: 작은 구슬의 이동, Time: 171ms, Memory: 15MB, Status: Passed - Codetree

--- 250527/작은 구슬의 이동/small-marble-movement.js
+++ 250527/작은 구슬의 이동/small-marble-movement.js
@@ -8,6 +8,9 @@ c = Number(c);
 
 // Please Write your code here.
 
+let newr=r-1
+let newc=c-1
+
 let dx=[-1,1,0,0]
 let dy=[0,0,1,-1]
 
@@ -21,9 +24,9 @@ let dir={
 let dirIdx=dir[d]
 
 for(let i=0; i<t; i++){
-    let nx=r+dx[dirIdx]
-    let ny=c+dy[dirIdx]
-    if(nx<1||nx>n||ny<1||ny>n){
+    let nx=newr+dx[dirIdx]
+    let ny=newc+dy[dirIdx]
+    if(nx<0||nx>=n||ny<0||ny>=n){
         if(dirIdx===0){
             dirIdx=1
         }else if (dirIdx === 1) {
@@ -34,9 +37,9 @@ for(let i=0; i<t; i++){
             dirIdx = 2;
         }
     }else{
-        r=nx
-        c=ny
+        newr=nx
+        newc=ny
     }
 }
 
-console.log(r,c)
+console.log(newr+1,newc+1)
