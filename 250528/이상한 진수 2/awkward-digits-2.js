0fa829ea10b7c8907c3740254307918a4ebda3ae
Son <nobleser@naver.com>
2025-05-28 23:51:30 +0900

    Title: 이상한 진수 2, Time: 173ms, Memory: 15MB, Status: Wrong Answer - Codetree

---
+++ 250528/이상한 진수 2/awkward-digits-2.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const a = input[0];
+
+// Please Write your code here.
+
+
+let maxNum=Infinity
+// for(let x of input)
+
+console.log(a)
