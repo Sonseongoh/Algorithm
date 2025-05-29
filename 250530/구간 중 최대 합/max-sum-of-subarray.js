04b8f2149dd6b9d7f375f8970a6d12c9a028a3d0
Son <nobleser@naver.com>
2025-05-30 00:56:58 +0900

    Title: 구간 중 최대 합, Time: 163ms, Memory: 15MB, Status: Wrong Answer - Codetree

---
+++ 250530/구간 중 최대 합/max-sum-of-subarray.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const [n, k] = input[0].split(' ').map(Number);
+const arr = input[1].split(' ').map(Number);
+
+// Please Write your code here.
+
+let answer=-Infinity;
+
+for(let i=0; i<n-k; i++){
+    let maxVal=0
+    for(let j=i; j<i+k; j++){
+        maxVal+=arr[j]
+    }
+    if(maxVal>answer) answer=maxVal
+}
+
+console.log(answer)
