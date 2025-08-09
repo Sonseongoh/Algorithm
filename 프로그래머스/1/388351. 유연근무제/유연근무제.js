bd3396bef0653f7ada2b676d51a6fd57dc79e25c
Son <nobleser@naver.com>
2025-08-09 23:22:29 +0900

    [level 1] Title: 유연근무제, Time: 3.70 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/1/388351. 유연근무제/유연근무제.js
+++ 프로그래머스/1/388351. 유연근무제/유연근무제.js
@@ -1,34 +1,27 @@
 function solution(schedules, timelogs, startday) {
-    function timeTranslate(time){
+    function timeCheck(time){
         let hour=Math.floor(time/100)
-        let min=time%100
-        min+=10
+        let min=time%100+10
         if(min>=60){
             hour+=1
             min-=60
         }
         return hour*100+min
     }
-    
     let answer = 0;
     let days=['월','화','수','목','금','토','일']
     
     for(let i=0; i<schedules.length; i++){
-        let limit=timeTranslate(schedules[i])
-        let ok =true
-        
-        for(let j=0; j<7; j++){
+        let flag=0
+        let limit=timeCheck(schedules[i])
+        for(let j=0; j<timelogs[0].length; j++){
             let start=days[(startday-1+j)%7]
-            if(start==='토' || start==='일'){
-                continue;
-            }
-            
-            if(timelogs[i][j]>limit){
-                ok=false
-                
+            if(start==='토'||start==='일') continue;
+            if(timelogs[i][j]<=limit){
+                flag++
             }
         }
-         if(ok) answer++
+        if(flag===5) answer++
     }
     
     return answer;
