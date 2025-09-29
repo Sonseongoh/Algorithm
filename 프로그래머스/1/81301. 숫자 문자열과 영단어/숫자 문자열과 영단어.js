eb39d33281ed29b077b0a1de6a0739e19f12676d
Son <nobleser@naver.com>
2025-09-30 03:37:39 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.12 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -1,10 +1,11 @@
 function solution(s) {
-    let words = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
-   
-    for(let i =0; i<words.length; i++){
-      
-            s=s.replaceAll(words[i],i)
-
+    let numbers=['zero','one','two','three','four','five','six','seven','eight','nine']
+    
+    for(let i=0; i<numbers.length; i++){
+        if(s.includes(numbers[i])){
+            s=s.replaceAll(numbers[i],i)
+        }
+       
     }
     return Number(s);
 }
