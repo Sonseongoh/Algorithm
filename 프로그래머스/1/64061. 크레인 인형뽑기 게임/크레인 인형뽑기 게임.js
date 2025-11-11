16f1a13a18a3e935b1b3d063e7c399aca45882d8
Son <nobleser@naver.com>
2025-11-12 00:11:39 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 30.17 ms, Memory: 36.6 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
@@ -1,27 +1,19 @@
 function solution(board, moves) {
-    let answer = 0
-    let basket=[]
-    for (let i = 0; i < moves.length; i++) {
-        let col=moves[i]-1
-        for(let row =0; row<board.length; row++){
-            if(board[row][col]!==0){
-                basket.push(board[row][col])
-                board[row][col]=0
-                
-                if(basket.at(-1)===basket.at(-2)){
-                    basket.pop()
-                    basket.pop()
+    let answer = 0;
+    let container=[]
+    for(let i=0; i<moves.length; i++){
+        for(let row=0; row<board.length; row++){
+            if(board[row][moves[i]-1]!==0){
+                container.push(board[row][moves[i]-1])
+                board[row][moves[i]-1]=0
+                 if(container.at(-1)===container.at(-2)){
+                    container.pop()
+                    container.pop()
                     answer+=2
-                }
-                break;
+                    }
+                    break
             }
         }
-    }
+    } 
     return answer;
 }
-
-// 1. moves[i], moves.length-1 사용 
-// 2. basket =[] 에 담기
-// 3. board는 board.length 만큼의 정사각형
-// 4. board[i][0] 에서 i 가 증가하면서 0이 아닌걸 찾고 그걸 basket에 담은 다음 0으로 변경
-// 5. basket에서는 연속되는 숫자가 들어가면 삭제해줘야함, 그리고 삭제 했다면 answer+=2
